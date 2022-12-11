24b78287dd8b8316670ff14cdad44bdd2d05e1ef
AbmaelSouza <abmaelpsouza@gmail.com>
2022-12-11 11:37:38 -0300

    check if day off is on

--- Cor-dias.js
+++ Cor-dias.js
@@ -11,6 +11,7 @@ var proxfolga;
 var proxfolgadata;
 var data = new Date();
 data.setDate(data.getDate())
+data.setHours(0,0,0, 0)
 var diasem = data.getDay();
 var dif = data.getTime() - d.getTime();
 var diasdif = Math.ceil( dif / 1000 / 60 / 60 / 24 ) ;
@@ -25,13 +26,22 @@ for (let i = 0;i <= diasdif/2;i = i+2){
 for (let i = 0; i <= folgas.length;i++){
     let a = folgas[i].getTime()-data.getTime();
     if (a>0){
+
         proxfolga = folgas[i];
         break
     }
 }
 proxfolgadata = Math.ceil((proxfolga.getTime() - data.getTime())/ 1000 / 60 / 60 / 24 );
 
-
+function checkfolga(){
+    for (let i = 0; i < folgas.length; i++) {
+        if (folgas[i].getTime() === data.getTime()){
+            return true;
+        }
+    }
+}
+var texto = "A "
+var texto2= ""
 $( document ).ready( function () {
     var diasemana = $( "#diasemana" );
     diasemana.append(dias[diasem][0].toUpperCase() + dias[diasem].slice(1));
@@ -39,7 +49,12 @@ $( document ).ready( function () {
     var diasfolga = $("#diasfolga");
     cortext.append(cor[diasem].toUpperCase());
     cortext.css("color", cortexto[cor[diasem]])
-    diasfolga.append(proxfolgadata);
-
-
+     if(checkfolga()) {
+         diasfolga.append( "A sua folga é HOJE e "  );
+         texto = "a "
+     }
+     if(proxfolgadata>1){
+         texto2 = "s"
+     }
+    diasfolga.append( texto +"próxima folga é em " + proxfolgadata + " dia"+texto2 );
 } );
